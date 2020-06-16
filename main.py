b9a8a5e5a19e463d697f9e3206ab6a8e913d65ac
Daniel Thompson <DanThomp507@gmail.com>
2020-06-16 13:03:31 -0400

    implemented csv functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 github_page = requests.get('https://github.com/trending')
 
@@ -11,6 +12,14 @@ repo_list = repo.find_all(class_='Box-row')
 
 print(len(repo_list))
 
+# CSV functionality
+file_name = 'github_trending_today.csv'
+
+f = csv.writer(open(file_name, 'w', newline=''))
+
+f.writerow(['Developer', 'Repo Name', 'Number of Stars'])
+
+# iterate through repo list to get data points for csv, then write to file
 for repo in repo_list:
     full_repo_name = repo.find(class_='h3 lh-condensed').text.split()
 
@@ -19,3 +28,5 @@ for repo in repo_list:
     repo_name = full_repo_name[2]
 
     stars = repo.find(class_='octicon octicon-star').parent.text.strip()
+    
+    f.writerow([developer, repo_name, stars])
