1c7616fdb83c4a349b15529cff39b86294a0778e
Daniel Thompson <DanThomp507@gmail.com>
2020-06-16 12:46:16 -0400

    managed to select all the proper elements - github trending repos

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 github_page = requests.get('https://github.com/trending')
-print(github_page)
+# print(github_page)
 
 soup_object = BeautifulSoup(github_page.text, 'html.parser')
-print(soup_object)
+# print(soup_object)
+
+repo = soup_object.find(class_='explore-pjax-container container-lg p-responsive pt-6')
+# print(repo)
+
+repo_list = repo.find_all(class_='Box-row')
+
+print(len(repo_list))
