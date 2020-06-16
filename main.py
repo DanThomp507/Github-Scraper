d1b7bf3408dfc8a3e60aa85a611693098ca7b6e4
Daniel Thompson <DanThomp507@gmail.com>
2020-06-16 12:46:54 -0400

    clean up

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 github_page = requests.get('https://github.com/trending')
-# print(github_page)
 
 soup_object = BeautifulSoup(github_page.text, 'html.parser')
-# print(soup_object)
 
 repo = soup_object.find(class_='explore-pjax-container container-lg p-responsive pt-6')
-# print(repo)
 
 repo_list = repo.find_all(class_='Box-row')
 
